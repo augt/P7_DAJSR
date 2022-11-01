5c537e9050bf90df636a12198e616737db0e378b
Augustin Tacquet <augu.tacquet@gmail.com>
2022-11-01 12:37:22 +0100

    Prepared code for receving tags lists

--- scripts.js
+++ scripts.js
@@ -2,15 +2,26 @@ let recipesFullList = recipes;
 
 const recipesBlock = document.getElementById("recipes-block");
 
+// display all recipes
+
 function generateRecipesCardsList(recipesList) {
   recipesList.forEach(function (recipeItem) {
+    let descriptionToArray = recipeItem.description.split(" ");
+    if (descriptionToArray.length > 30) {
+      let shortenedDescriptionArray = descriptionToArray.slice(0, 30);
+      shortenedDescriptionArray.push("...");
+      let stringifiedshortenedDescriptionArray =
+        shortenedDescriptionArray.join(" ");
+      recipeItem.description = stringifiedshortenedDescriptionArray;
+    }
+
     const templateRecipeCard = `
         <div class="recipe-card">
             <div class="empty-photo">
             </div>
             <div>
                 <div class="recipe-header">
-                    <div>
+                    <div class="recipe-name">
                     ${recipeItem.name}
                     </div>
                     <div class="duration">
@@ -57,3 +68,51 @@ function generateRecipesCardsList(recipesList) {
 }
 
 generateRecipesCardsList(recipesFullList);
+
+// display tag filters lists
+
+const ingredientsList = [];
+
+recipesFullList.forEach(function (recipeItem) {
+  recipeItem.ingredients.forEach(function (ingredientItem) {
+    const existingIngredient = ingredientsList.find(
+      (element) => element === ingredientItem.ingredient
+    );
+
+    if (!existingIngredient) {
+      ingredientsList.push(ingredientItem.ingredient);
+    }
+  });
+});
+
+console.log(ingredientsList);
+
+const appliancesList = [];
+
+recipesFullList.forEach(function (recipeItem) {
+  const existingAppliance = appliancesList.find(
+    (element) => element === recipeItem.appliance
+  );
+
+  if (!existingAppliance) {
+    appliancesList.push(recipeItem.appliance);
+  }
+});
+
+console.log(appliancesList);
+
+const utensilsList = [];
+
+recipesFullList.forEach(function (recipeItem) {
+  recipeItem.utensils.forEach(function (utensilItem) {
+    const existingUtensil = utensilsList.find(
+      (element) => element === utensilItem
+    );
+
+    if (!existingUtensil) {
+      utensilsList.push(utensilItem);
+    }
+  });
+});
+
+console.log(utensilsList);
